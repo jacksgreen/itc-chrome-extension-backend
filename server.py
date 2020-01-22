a790b32e322e6599c3d43c42aebede507a26542d
gabegor <gabegor94@gmail.com>
2020-01-22 18:34:42 +0200

    added compo

--- server.py
+++ server.py
@@ -11,10 +11,26 @@ CORS(app)
 
 
 RESPONSE = {'msg': 'Success',
-            'mainProduct': {'brand': '2UNDR', 'title': "2UNDR Men's Luxury V-Neck Tees", 'composition': ['modal', 'polyester'], 'type': 'T-Shirts', 'parent_type': 'Shirts', 'price': 39.98, 'features': ['No Closure closure', 'Classic V-neck', 'Modal/Poly blend', 'Ordr resistant material'], 'ecoscore': 1, 'photoUrl': 'https://images-na.ssl-images-amazon.com/images/I/71h1rgjhwvL._AC_UX679_.jpg', 'url': 'https://www.amazon.com/2UNDR-Mens-Luxury-V-Neck-X-Large/dp/B07JPB1BP1?pf_rd_p=700385e0-19d0-4b4c-89bf-06e23d6f1594&pd_rd_wg=eSwIV&pf_rd_r=VFZ0Z500YC2MQNGBJPD2&ref_=pd_gw_cr_simh&pd_rd_w=qoyy2&pd_rd_r=01e7c99b-25a4-4083-a5af-7ad97e1cf51b'},
-            'firstSuggestion': {'brand': '2UNDR', 'title': "2UNDR Men's Luxury V-Neck Tees", 'composition': ['modal', 'polyester'], 'type': 'T-Shirts', 'parent_type': 'Shirts', 'price': 39.98, 'features': ['No Closure closure', 'Classic V-neck', 'Modal/Poly blend', 'Ordr resistant material'], 'ecoscore': 1, 'photoUrl': 'https://images-na.ssl-images-amazon.com/images/I/71h1rgjhwvL._AC_UX679_.jpg', 'url': 'https://www.amazon.com/2UNDR-Mens-Luxury-V-Neck-X-Large/dp/B07JPB1BP1?pf_rd_p=700385e0-19d0-4b4c-89bf-06e23d6f1594&pd_rd_wg=eSwIV&pf_rd_r=VFZ0Z500YC2MQNGBJPD2&ref_=pd_gw_cr_simh&pd_rd_w=qoyy2&pd_rd_r=01e7c99b-25a4-4083-a5af-7ad97e1cf51b'},
-            'secondSuggestion': {'brand': '2UNDR', 'title': "2UNDR Men's Luxury V-Neck Tees", 'composition': ['modal', 'polyester'], 'type': 'T-Shirts', 'parent_type': 'Shirts', 'price': 39.98, 'features': ['No Closure closure', 'Classic V-neck', 'Modal/Poly blend', 'Ordr resistant material'], 'ecoscore': 1, 'photoUrl': 'https://images-na.ssl-images-amazon.com/images/I/71h1rgjhwvL._AC_UX679_.jpg', 'url': 'https://www.amazon.com/2UNDR-Mens-Luxury-V-Neck-X-Large/dp/B07JPB1BP1?pf_rd_p=700385e0-19d0-4b4c-89bf-06e23d6f1594&pd_rd_wg=eSwIV&pf_rd_r=VFZ0Z500YC2MQNGBJPD2&ref_=pd_gw_cr_simh&pd_rd_w=qoyy2&pd_rd_r=01e7c99b-25a4-4083-a5af-7ad97e1cf51b'},
-            'thirdSuggestion': {'brand': '2UNDR', 'title': "2UNDR Men's Luxury V-Neck Tees", 'composition': ['modal', 'polyester'], 'type': 'T-Shirts', 'parent_type': 'Shirts', 'price': 39.98, 'features': ['No Closure closure', 'Classic V-neck', 'Modal/Poly blend', 'Ordr resistant material'], 'ecoscore': 1, 'photoUrl': 'https://images-na.ssl-images-amazon.com/images/I/71h1rgjhwvL._AC_UX679_.jpg', 'url': 'https://www.amazon.com/2UNDR-Mens-Luxury-V-Neck-X-Large/dp/B07JPB1BP1?pf_rd_p=700385e0-19d0-4b4c-89bf-06e23d6f1594&pd_rd_wg=eSwIV&pf_rd_r=VFZ0Z500YC2MQNGBJPD2&ref_=pd_gw_cr_simh&pd_rd_w=qoyy2&pd_rd_r=01e7c99b-25a4-4083-a5af-7ad97e1cf51b'},
+            'mainProduct': {'brand': '2UNDR', 'title': "2UNDR Men's Luxury V-Neck Tees",
+                            'composition': ['modal', 'polyester'], 'type': 'T-Shirts',
+                            'parent_type': 'Shirts', 'price': 39.98,
+                            'features': ['No Closure closure', 'Classic V-neck', 'Modal/Poly blend', 'Ordr resistant material'],
+                            'ecoscore': 5, 'photoUrl': 'https://images-na.ssl-images-amazon.com/images/I/71h1rgjhwvL._AC_UX679_.jpg', 'url': 'https://www.amazon.com/2UNDR-Mens-Luxury-V-Neck-X-Large/dp/B07JPB1BP1?pf_rd_p=700385e0-19d0-4b4c-89bf-06e23d6f1594&pd_rd_wg=eSwIV&pf_rd_r=VFZ0Z500YC2MQNGBJPD2&ref_=pd_gw_cr_simh&pd_rd_w=qoyy2&pd_rd_r=01e7c99b-25a4-4083-a5af-7ad97e1cf51b'},
+            'firstSuggestion': {'brand': '2UNDR', 'title': "2UNDR Men's Luxury V-Neck Tees",
+                                'composition': ['modal', 'polyester'], 'type': 'T-Shirts',
+                                'parent_type': 'Shirts', 'price': 43.99,
+                                'features': ['No Closure closure', 'Classic V-neck', 'Modal/Poly blend', 'Ordr resistant material'],
+                                'ecoscore': 9, 'photoUrl': 'https://images-na.ssl-images-amazon.com/images/I/71h1rgjhwvL._AC_UX679_.jpg', 'url': 'https://www.amazon.com/2UNDR-Mens-Luxury-V-Neck-X-Large/dp/B07JPB1BP1?pf_rd_p=700385e0-19d0-4b4c-89bf-06e23d6f1594&pd_rd_wg=eSwIV&pf_rd_r=VFZ0Z500YC2MQNGBJPD2&ref_=pd_gw_cr_simh&pd_rd_w=qoyy2&pd_rd_r=01e7c99b-25a4-4083-a5af-7ad97e1cf51b'},
+            'secondSuggestion': {'brand': '2UNDR', 'title': "2UNDR Men's Luxury V-Neck Tees",
+                                 'composition': ['modal', 'polyester'], 'type': 'T-Shirts',
+                                 'parent_type': 'Shirts', 'price': 41.99,
+                                 'features': ['No Closure closure', 'Classic V-neck', 'Modal/Poly blend', 'Ordr resistant material'],
+                                 'ecoscore': 8, 'photoUrl': 'https://images-na.ssl-images-amazon.com/images/I/71h1rgjhwvL._AC_UX679_.jpg', 'url': 'https://www.amazon.com/2UNDR-Mens-Luxury-V-Neck-X-Large/dp/B07JPB1BP1?pf_rd_p=700385e0-19d0-4b4c-89bf-06e23d6f1594&pd_rd_wg=eSwIV&pf_rd_r=VFZ0Z500YC2MQNGBJPD2&ref_=pd_gw_cr_simh&pd_rd_w=qoyy2&pd_rd_r=01e7c99b-25a4-4083-a5af-7ad97e1cf51b'},
+            'thirdSuggestion': {'brand': '2UNDR', 'title': "2UNDR Men's Luxury V-Neck Tees",
+                                'composition': ['modal', 'polyester'], 'type': 'T-Shirts',
+                                'parent_type': 'Shirts', 'price': 30.00,
+                                'features': ['No Closure closure', 'Classic V-neck', 'Modal/Poly blend', 'Ordr resistant material'],
+                                'ecoscore': 6, 'photoUrl': 'https://images-na.ssl-images-amazon.com/images/I/71h1rgjhwvL._AC_UX679_.jpg', 'url': 'https://www.amazon.com/2UNDR-Mens-Luxury-V-Neck-X-Large/dp/B07JPB1BP1?pf_rd_p=700385e0-19d0-4b4c-89bf-06e23d6f1594&pd_rd_wg=eSwIV&pf_rd_r=VFZ0Z500YC2MQNGBJPD2&ref_=pd_gw_cr_simh&pd_rd_w=qoyy2&pd_rd_r=01e7c99b-25a4-4083-a5af-7ad97e1cf51b'},
             }
 
 chromeURL = ''
