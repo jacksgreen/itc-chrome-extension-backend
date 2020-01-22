c65e4bf6fb0383a69d6dfa85b091182baa7186c2
gabegor <gabegor94@gmail.com>
2020-01-22 10:46:22 +0200

    added msg and clears url

--- server.py
+++ server.py
@@ -10,30 +10,36 @@ app = Flask(__name__)
 CORS(app)
 
 
-RESPONSE = {
-    'mainProduct': {'price': '$5', 'titleName': 'White V-neck', 'subTitle': 'Amazon product 1',
-                    'ecoScore': '6', 'photoUrl': 'https://shirtsofcotton.com/en/media/catalog/product/cache/10/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/o/soc.02-wit-t-shirt-basic-v-hals.jpg',
-                    'details': '100% cotton'},
-    'firstSuggestion': {'price': '$7', 'titleName': 'White T-Shirt 1', 'subTitle': 'Amazon product 2',
-                        'ecoScore': '3', 'photoUrl': 'https://cowesclothing.com/uploaded/thumbnails/16823185/rna1-white-(A)_16823185_1080xauto.png',
-                        'details': '90% cotton 10% Plastic'},
-    'secondSuggestion': {'price': '$2', 'titleName': 'White T-Shirt 2', 'subTitle': 'Amazon product 3 ',
-                         'ecoScore': '5', 'photoUrl': 'https://5.imimg.com/data5/YB/QU/MY-24671135/blank-t-shirt-500x500.jpg',
-                         'details': '70% cotton'},
-    'thirdSuggestion': {'price': '$3', 'titleName': 'White T-Shirt 3', 'subTitle': 'Amazon product 4',
-                        'ecoScore': '1', 'photoUrl': 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1553119996-everlane-1553119988.jpg',
-                        'details': '10% cotton'},
-}
+RESPONSE = {'msg': 'Success',
+            'mainProduct': {'price': '$5', 'titleName': 'White V-neck', 'subTitle': 'Amazon product 1',
+                            'ecoScore': '6', 'photoUrl': 'https://shirtsofcotton.com/en/media/catalog/product/cache/10/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/o/soc.02-wit-t-shirt-basic-v-hals.jpg',
+                            'details': '100% cotton'},
+            'firstSuggestion': {'price': '$7', 'titleName': 'White T-Shirt 1', 'subTitle': 'Amazon product 2',
+                                'ecoScore': '3', 'photoUrl': 'https://cowesclothing.com/uploaded/thumbnails/16823185/rna1-white-(A)_16823185_1080xauto.png',
+                                'details': '90% cotton 10% Plastic'},
+            'secondSuggestion': {'price': '$2', 'titleName': 'White T-Shirt 2', 'subTitle': 'Amazon product 3 ',
+                                 'ecoScore': '5', 'photoUrl': 'https://5.imimg.com/data5/YB/QU/MY-24671135/blank-t-shirt-500x500.jpg',
+                                 'details': '70% cotton'},
+            'thirdSuggestion': {'price': '$3', 'titleName': 'White T-Shirt 3', 'subTitle': 'Amazon product 4',
+                                'ecoScore': '1', 'photoUrl': 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1553119996-everlane-1553119988.jpg',
+                                'details': '10% cotton'},
+            }
 
-chromeURL =''
+chromeURL = ''
+
 
 def obj_dict(obj):
     return obj.__dict__
 
+
 @app.route('/', methods=['GET'])
 def getResponse():
-    global RESPONSE
-    return json.dumps(RESPONSE, default=obj_dict)
+    global RESPONSE, chromeURL
+    if(chromeURL != ''):
+        chromeURL = ''
+        return json.dumps(RESPONSE, default=obj_dict)
+    else:
+        return {'msg': 'No URL'}
 
 
 @app.route('/url', methods=['GET'])
