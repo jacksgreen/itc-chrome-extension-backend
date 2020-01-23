e0fb20cff40576ddad3201f7f12f1ee3d6115be0
Yinon Vahab <yinonv@gmail.com>
2020-01-23 12:37:54 +0200

    pushing to heroku

--- server.py
+++ server.py
@@ -58,5 +58,5 @@ def get_data():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 2700))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='127.0.0.1', port=port, debug=True)
